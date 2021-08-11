987427c64c3c4ac73230061fb34d521427e3a36a
Ted Potter <tpotter@financialforce.com>
2021-08-11 11:35:14 -0400

    Update to include link to Contact page

--- force-app/main/default/lwc/personaNameCard/personaNameCard.js
+++ force-app/main/default/lwc/personaNameCard/personaNameCard.js
@@ -5,9 +5,12 @@ import NAME_FIELD from '@salesforce/schema/User.Name';
 import TITLE_FIELD from '@salesforce/schema/User.Title';
 import IMAGE_FIELD from '@salesforce/schema/User.MediumPhotoUrl';
 import BANNER_IMAGE_FIELD from '@salesforce/schema/User.BannerPhotoUrl';
+import getContactPageURL  from '@salesforce/apex/GetData.getContactPageURL';
 export default class userDetails extends LightningElement {
      @track error ;
+     @track hasRendered = false;
      @track name;
+     @track ContactURL;
      @wire(getRecord, {
          recordId: USER_ID,
          fields: [NAME_FIELD,TITLE_FIELD,IMAGE_FIELD]
@@ -22,6 +25,32 @@ export default class userDetails extends LightningElement {
              this.title = data.fields.Title.value;
              this.MediumPhotoUrl = data.fields.MediumPhotoUrl.value;
          }
-         
-     }
+     };
+    renderedCallback(){
+        if(this.hasRendered == false){
+            //alert('get url');
+            getContactPageURL({
+                userId: USER_ID
+            }).then((result)=>{
+                //alert(result);
+                if(result==null) {
+                    // No valid Contact record - set to blank
+                    this.ContactURL = '';   
+                    //alert(' setting to blank');     
+                } else {
+                    // We DO have a valid Contact record
+                    this.ContactURL=result;
+                    //alert('set up anchor on image: ' + this.ContactURL);
+                }
+            }).catch((error)=>{
+                console.log("Error-in getContatPageURL. Error: " + error.name);
+                this.showToast('ERROR','Something went wrong. Error: ' + error.body.message,'error');
+            }); 
+        this.hasRendered = true;
+        }
+    }
+
+    handleImageClick(event){
+        window.open(this.ContactURL, '_blank');
+    }
  }
